4f872600e391dc1a00436cb4b0ebc01b7424b70c
daiisntos <daiisntos@gmail.com>
2024-04-15 23:53:26 -0300

    Insert atualization of server

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const { v4: uuidv4} = require('uuid');
+const router = express.Router();
 
 const app = express();
 const door = 3333;
 
-function showDoor() {
-    console.log("Server created and running on the port:", door);
+function createWoman(request,response){
+    const newWoman = {
+        id: uuidv4(),
+        name: request.body.name,
+        image: request.body.image,
+        minibio: request.body.minibio
+
+    }
+    womans.push(newWoman);
+    response.json(womans);
 }
 
+function showDoor() {
+    console.log("Servidor criado e rodando na Door", door);
+}
+
+
+app.use(router.post('womans'), createWoman);
 app.listen(door, showDoor);
